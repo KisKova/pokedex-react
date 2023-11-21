71dbde559b5ede4dd5cf037c93ebf7515ccaeb2b
marcellkovacs <kovacsmarci927@gmail.com>
2023-11-21 10:21:21 +0100

    Add the style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Pokemon from "./pages/Pokemon";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/pokemon/:id" element={<Pokemon />} />
       </Routes>
     </Router>
   );
