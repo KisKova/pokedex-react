8db69e6e276cc65f9b8a80e47063425d30678ec7
marcellkovacs <kovacsmarci927@gmail.com>
2023-11-21 10:06:20 +0100

    Deployed website update with BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import {Route, Router, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router basename='/pokedex-react'>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
